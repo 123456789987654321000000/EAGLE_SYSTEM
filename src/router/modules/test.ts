527a8bbe2713bd22002372e0444f168f17d6e9e7
BarryBI3555 <bisheng0915@163.com>
2026-03-23 23:25:52 +0800

    first commit

---
+++ src/router/modules/test.ts
@@ -0,0 +1,25 @@
+import { AppRouteRecord } from '@/types/router'
+
+export const testRoutes: AppRouteRecord = {
+    path: '/test',
+    name: 'test',
+    component: '/index/index',
+    meta: {
+        title: 'menus.test.title',
+        icon: 'ri:user-3-line',
+        roles: ['R_SUPER', 'R_ADMIN']
+    },
+    children: [
+        {
+            path: 'test1',
+            name: 'test1',
+            component: '/test/test1',
+            meta: {
+                title: 'menus.test.user',
+                isHide: false,
+                keepAlive: true,
+                isHideTab: false
+            }
+        }
+    ]
+}
---
+++ src/views/test/test1.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+6666666666666666666666666
+</template>
+
+<style scoped lang="scss">
+
+</style>
